930b3f7050420c230306b52d0bd2a6d04c748840
TaneriX07 <tanerivince@gmail.com>
2022-05-16 21:12:13 +0800

    Bug fixes

--- index.js
+++ index.js
@@ -18,9 +18,58 @@ app.get('/', (req, res) => {
   res.sendStatus(200);
 });
 
-// When user interact with bot, Line will send a POST req to webhook URL
+// when user interact with bot, Line will send a POST req to webhook URL
 app.post('/webhook', function (req, res) {
+  console.log('Received a message');
   res.send('HTTP POST request sent to the webhook URL!');
+  // If the user sends a message to your bot, send a reply message
+  if (req.body.events[0].type === 'message') {
+    // Message data, must be stringified
+    const dataString = JSON.stringify({
+      replyToken: req.body.events[0].replyToken,
+      messages: [
+        {
+          type: 'text',
+          text: 'Hello, user',
+        },
+        {
+          type: 'text',
+          text: 'May I help you?',
+        },
+      ],
+    });
+
+    // Request header
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + TOKEN,
+    };
+
+    // Options to pass into the request
+    const webhookOptions = {
+      hostname: 'api.line.me',
+      path: '/v2/bot/message/reply',
+      method: 'POST',
+      headers: headers,
+      body: dataString,
+    };
+
+    // Define request
+    const request = https.request(webhookOptions, (res) => {
+      res.on('data', (d) => {
+        process.stdout.write(d);
+      });
+    });
+
+    // Handle error
+    request.on('error', (err) => {
+      console.error(err);
+    });
+
+    // Send data
+    request.write(dataString);
+    request.end();
+  }
 });
 
 app.listen(PORT, () => {
