b962d7eef3663c23ebed0641b70bad7409134002
TaneriX07 <tanerivince@gmail.com>
2022-05-16 23:56:44 +0800

    Remove unnecessary files

--- test.js
+++
@@ -1,9 +0,0 @@
-let d = new Date(2022, 04, 16, 00, 00, 00).toUTCString();
-let d2 = new Date();
-var now = new Date();
-var utc = new Date(
-  now.getTime() + now.getTimezoneOffset() * 60000
-).toUTCString();
-console.log(d2.toUTCString());
-console.log(d2.toString());
-console.log(utc);
